daec35faf66bdacd2742ce831a7b7ffbc0d0e170
Eliazer Braun <adardesign@gmail.com>
2018-01-15 11:05:21 -0500

    added async to the build

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -12,7 +12,7 @@ const eslintFormatter = require('react-dev-utils/eslintFormatter');
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 const paths = require('./paths');
 const getClientEnvironment = require('./env');
-
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
 const publicPath = paths.servedPath;
@@ -256,6 +256,9 @@ module.exports = {
         minifyCSS: true,
         minifyURLs: true,
       },
+    }),
+    new ScriptExtHtmlWebpackPlugin({
+      defaultAttribute: 'async'
     }),
     // Makes some environment variables available to the JS code, for example:
     // if (process.env.NODE_ENV === 'production') { ... }. See `./env.js`.
